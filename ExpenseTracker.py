e0fe8b2645dba19df6ff52b0c08bd23592e2cf46
AdityaAc007 <156281556+AdityaAc007@users.noreply.github.com>
2024-06-10 20:05:11 +0530

    Update ExpenseTracker.py

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -1,25 +1,8 @@
 expenses = []
-expense1 = {'amount': '100.00', 'category': 'shirt'}
+expense1 = {'amount': '1000.00', 'category': 'shirt'}
 expenses.append(expense1)
-expense2 = {'amount': '50.25', 'category': 'groceries'}
+expense2 = {'amount': '300', 'category': 'groceries'}
 expenses.append(expense2)
-
-
-def removeExpense():
-  while True:
-    listExpenses()
-    print("What expense would you like to remove?")
-    try:
-      expenseToRemove = int(input("> "))
-      del expenses[expenseToRemove]
-      break
-    except:
-      print("Invalid input. Please try again.")
-
-
-def addExpense(amount, category):
-  expense = {'amount': amount, 'category': category}
-  expenses.append(expense)
 
 
 def printMenu():
@@ -28,6 +11,9 @@ def printMenu():
   print("2. Remove An Expense")
   print("3. List All Expenses")
 
+def addExpense(amount, category):
+  expense = {'amount': amount, 'category': category}
+  expenses.append(expense)
 
 def listExpenses():
   print("\nHere is a list of your expenses...")
@@ -37,6 +23,18 @@ def listExpenses():
     print("#", counter, " - ", expense['amount'], " - ", expense['category'])
     counter += 1
   print("\n\n")
+
+
+def removeExpense():
+   while True:
+      listExpenses()
+      print("What expense would you like to remove?")
+      try:
+        expenseToRemove = int(input("> "))
+        del expenses[expenseToRemove]
+        break
+      except:
+        print("Invalid input. Please try again.")
 
 
 if __name__ == "__main__":
